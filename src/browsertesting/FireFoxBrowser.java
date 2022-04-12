531ed4a9044b32437cc2ce42bfe9e458f6f4db5c
pinkhouse123 <wembley1400@gmail.com>
2022-04-12 15:49:00 +0100

    Homework week 10.5

---
+++ src/browsertesting/FireFoxBrowser.java
@@ -0,0 +1,42 @@
+package browsertesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxBrowser {
+    public static void main(String[] args) {
+        String baseUrl="https://courses.ultimateqa.com/users/sign_in";
+        System.setProperty("webdriver.gecko.driver","drivers/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        //Launch the URL
+        driver.get(baseUrl);
+
+        //Maximise window
+        driver.manage().window().maximize();
+
+        //Get and print the title of the page
+        String getTitle= driver.getTitle();
+        System.out.println(getTitle);
+
+        //Get Current URL
+        System.out.println("Current URL =" + driver.getCurrentUrl());
+
+
+        //Find the email field element
+        WebElement emailField = driver.findElement(By.id("user[email]"));
+        //Sending email to email field element
+        emailField.sendKeys("umipatel14@gmail.com");
+
+        //Find the password field element
+        WebElement passwordField = driver.findElement(By.name("user[password]"));
+        //Sending password to password field element
+        passwordField.sendKeys("umi123");
+
+        //Close the browser
+        driver . close();
+
+    }
+}
